0ec1aa3712b8341e1f3e8ce12523cd0d94c68aa8
David Knoll <david@davidknoll.me.uk>
2023-07-19 03:28:40 +0100

    Add downloading/transcoding to MP3

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const ytpl = require('ytpl');
 const ytdl = require('ytdl-core');
+const tmp = require('tmp-promise');
 const express = require('express');
 const { Podcast } = require('podcast');
+const ffmpeg = require('fluent-ffmpeg');
+const fsPromises = require('fs/promises');
 
 const port = process.env.PORT || 3000;
 const eplimit = process.env.EPISODE_LIMIT || 20;
@@ -25,12 +29,11 @@ Hello, I am youtube-podcast
   })
 
   .get('/feed/:id.rss', (req, res) => {
-    console.log(req.originalUrl);
+    console.log(new Date().toLocaleTimeString('en-GB') + ' ' + req.originalUrl);
     let feed;
 
     ytpl(req.params.id, { limit: eplimit })
       .then(playlist => {
-        // console.log(JSON.stringify(playlist, null, 2)); // DEBUG
         const myurl = new URL(req.originalUrl, `${req.protocol}://${req.hostname}:${port}`);
         feed = new Podcast({
           title: playlist.title,
@@ -48,8 +51,8 @@ Hello, I am youtube-podcast
 
         const itemPromises = playlist.items.map(item => ytdl.getInfo(item.id)
           .then(info => {
-            // console.log(JSON.stringify(info, null, 2)); // DEBUG
-            const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
+            const itemurl = new URL(myurl);
+            itemurl.pathname = `/item/${item.id}.mp3`;
             return {
               title: info.videoDetails.title,
               description: info.videoDetails.description,
@@ -59,9 +62,8 @@ Hello, I am youtube-podcast
               author: info.videoDetails.author.name,
               date: info.videoDetails.publishDate,
               enclosure: {
-                url: format.url,
-                size: format.contentLength,
-                type: format.mimeType,
+                url: itemurl.href,
+                type: 'audio/mpeg',
               },
               itunesAuthor: info.videoDetails.author.name,
               itunesSummary: info.videoDetails.description,
@@ -80,6 +82,35 @@ Hello, I am youtube-podcast
         return feed.buildXml('  ');
       })
       .then(rss => res.type('text/xml').send(rss))
+      .catch(e => res.status(500).type('text/plain').send(e.message));
+  })
+
+  .get('/item/:id.mp3', (req, res) => {
+    console.log(new Date().toLocaleTimeString('en-GB') + ' ' + req.originalUrl);
+    let inPath, outPath;
+
+    tmp.tmpName({ postfix: '.tmp' })
+      .then(tmpPath => new Promise((resolve, reject) => {
+        inPath = tmpPath;
+        ytdl(req.params.id, { quality: 'highestaudio', filter: 'audioonly' })
+          .on('error', reject)
+          .on('finish', resolve)
+          .pipe(fs.createWriteStream(inPath));
+      }))
+      .then(() => tmp.tmpName({ postfix: '.mp3' }))
+      .then(tmpPath => new Promise((resolve, reject) => {
+        outPath = tmpPath;
+        ffmpeg(inPath)
+          .audioCodec('libmp3lame')
+          .audioBitrate(320)
+          .noVideo()
+          .on('error', reject)
+          .on('end', resolve)
+          .save(outPath);
+      }))
+      .then(() => res.sendFile(outPath))
+      .then(() => fsPromises.unlink(inPath))
+      .then(() => fsPromises.unlink(outPath))
       .catch(e => res.status(500).type('text/plain').send(e.message));
   })
   .listen(port);
