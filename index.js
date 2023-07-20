1fc977685f8b06fffa6998fb7542f65842124980
David Knoll <david@davidknoll.me.uk>
2023-07-20 16:00:30 +0100

    add caching, convert to es module

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const ytpl = require('ytpl');
-const ytdl = require('ytdl-core');
-const tmp = require('tmp-promise');
-const express = require('express');
-const { Podcast } = require('podcast');
-const ffmpeg = require('fluent-ffmpeg');
-const fsPromises = require('fs/promises');
+import fs from 'fs';
+import tmp from 'tmp';
+import path from 'path';
+import ytpl from 'ytpl';
+import ytdl from 'ytdl-core';
+import express from 'express';
+import { Podcast } from 'podcast';
+import ffmpeg from 'fluent-ffmpeg';
+import fsPromises from 'fs/promises';
+import tmpPromise from 'tmp-promise';
+import { moveFile } from 'move-file';
 
 const debug = !!parseInt(process.env.DEBUG, 10);
 const port = process.env.PORT || 3000;
 const eplimit = process.env.EPISODE_LIMIT || 20;
+const cachedir = process.env.CACHE_DIR || tmp.dirSync().name;
+tmp.setGracefulCleanup();
+if (!path.isAbsolute(cachedir)) {
+  throw new Error('CACHE_DIR, if provided, must be absolute');
+}
 console.log(`youtube-podcast listening on port ${port}`);
 
 express()
+  // Just so there's something here
   .get('/', (req, res) => {
     res.send(
 `<!DOCTYPE html>
@@ -29,10 +38,12 @@ Hello, I am youtube-podcast
     );
   })
 
+  // Generate an RSS podcast feed from a YouTube playlist ID
   .get('/feed/:id.rss', (req, res) => {
     console.log(new Date().toLocaleTimeString('en-GB') + ' ' + req.originalUrl);
     let feed;
 
+    // Request data about the playlist
     ytpl(req.params.id, { limit: eplimit })
       .then(playlist => {
         if (debug) { console.log(JSON.stringify(playlist, null, 2)); }
@@ -41,8 +52,8 @@ Hello, I am youtube-podcast
           title: playlist.title,
           description: playlist.description,
           generator: 'youtube-podcast',
-          feedUrl: myurl.href,
-          siteUrl: playlist.url,
+          feedUrl: myurl.href,    // This feed itself
+          siteUrl: playlist.url,  // Include the original YouTube URL
           imageUrl: playlist.bestThumbnail.url,
           author: playlist.author.name,
           pubDate: playlist.lastUpdated,
@@ -51,9 +62,11 @@ Hello, I am youtube-podcast
           itunesImage: playlist.bestThumbnail.url,
         });
 
+        // Request further data about all the items in the playlist
         const itemPromises = playlist.items.map(item => ytdl.getInfo(item.id)
           .then(info => {
             if (debug) { console.log(JSON.stringify(info, null, 2)); }
+            // Base the item URLs on that of the feed, as they are also served by this app
             const itemurl = new URL(myurl);
             itemurl.pathname = `/item/${item.id}.mp3`;
             itemurl.searchParams.set('list', playlist.id);
@@ -66,8 +79,8 @@ Hello, I am youtube-podcast
               author: info.videoDetails.author.name,
               date: info.videoDetails.publishDate,
               enclosure: {
-                url: itemurl.href,
-                type: 'audio/mpeg',
+                url: itemurl.href,  // Refers to the actual audio for the episode
+                type: 'audio/mpeg', // We always deliver it in MP3 here
               },
               itunesAuthor: info.videoDetails.author.name,
               itunesSummary: info.videoDetails.description,
@@ -89,15 +102,24 @@ Hello, I am youtube-podcast
       .catch(e => res.status(500).type('text/plain').send(e.message));
   })
 
-  .get('/item/:id.mp3', (req, res) => {
+  // Get an episode from the cache, if present
+  .get('/item/:id.mp3', (req, res, next) => {
     console.log(new Date().toLocaleTimeString('en-GB') + ' ' + req.originalUrl);
+    fsPromises.access(`${cachedir}/${req.params.id}.mp3`, fs.constants.R_OK)
+      .then(() => res.sendFile(`${cachedir}/${req.params.id}.mp3`))
+      .catch(e => next());
+
+  // Otherwise, download and convert it, add it to the cache
+  }, (req, res) => {
     let inPath, outPath, info, playlist;
 
-    tmp.tmpName({ postfix: '.tmp' })
+    // Download will first be saved to a temporary file
+    tmpPromise.tmpName({ postfix: '.tmp' })
       .then(tmpPath => new Promise((resolve, reject) => {
         inPath = tmpPath;
         if (debug) { console.log(inPath); }
         ytdl(req.params.id, { quality: 'highestaudio', filter: 'audioonly' })
+          // Metadata is retrieved so we can tag the converted MP3
           .on('info', (videoInfo, videoFormat) => {
             info = videoInfo.videoDetails;
             if (debug) {
@@ -105,6 +127,7 @@ Hello, I am youtube-podcast
               console.log(JSON.stringify(videoFormat, null, 2));
             }
           })
+          // Just for logging- client doesn't see this
           .on('progress', (chunkBytes, doneBytes, totalBytes) => {
             const progress = (doneBytes * 100) / totalBytes;
             if (progress % 10 < 0.05) {
@@ -116,13 +139,24 @@ Hello, I am youtube-podcast
           .pipe(fs.createWriteStream(inPath));
       }))
 
-      .then(() => ytpl(req.query.list, { limit: eplimit }))
-      .then(ytplResult => {
-        playlist = ytplResult;
-        if (debug) { console.log(JSON.stringify(playlist, null, 2)); }
-      })
+      // Get playlist metadata useful for tagging the MP3, if we have a list ID
+      .then(() =>
+        ytpl(req.query.list, { limit: eplimit })
+          .then(ytplResult => {
+            playlist = ytplResult;
+            if (debug) { console.log(JSON.stringify(playlist, null, 2)); }
+          })
+          .catch (e => {
+            playlist = {
+              title: '',
+              author: { name: '' },
+            };
+          })
+      )
 
-      .then(() => tmp.tmpName({ postfix: '.mp3' }))
+      // Convert from whatever format we managed to download, to MP3
+      // This requires ffmpeg to be installed on the same host or container as Node
+      .then(() => tmpPromise.tmpName({ postfix: '.mp3' }))
       .then(tmpPath => new Promise((resolve, reject) => {
         outPath = tmpPath;
         if (debug) { console.log(outPath); }
@@ -151,9 +185,10 @@ Hello, I am youtube-podcast
           .save(outPath);
       }))
 
-      .then(() => res.sendFile(outPath))
+      // Successful download, cache it
+      .then(() => moveFile(outPath, `${cachedir}/${req.params.id}.mp3`))
+      .then(() => res.sendFile(`${cachedir}/${req.params.id}.mp3`))
       .then(() => fsPromises.unlink(inPath))
-      .then(() => fsPromises.unlink(outPath))
       .catch(e => res.status(500).type('text/plain').send(e.message));
   })
   .listen(port);
